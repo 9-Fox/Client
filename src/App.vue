e0c4a566bfcc4ac5084ead2ae7bec8ae7bcb6e6c
samuelzega <samuel.zegaa@gmail.com>
2020-01-16 21:03:31 +0700

    tampilannya udah

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,44 @@
+
 <template>
-  
+  <div >
+    <b-container class="bv-example-row mb-3">
+      <b-row cols="2" align-h="around">
+          <MainCard></MainCard>
+          <MainCard></MainCard>
+          <MainCard></MainCard>
+          <MainCard></MainCard>
+          <MainCard></MainCard>
+      </b-row>
+      <b-row></b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
+import MainCard from "./components/MainCard";
 export default {
-name: "App"
-}
+  name: "App",
+  components: {
+    MainCard
+  },
+  data(){
+    return {
+      sample: 'ch'
+    }
+  },
+  methods:{
+    printSample(payload){
+      console.log('masuk ga?')
+      this.sample = payload
+    }
+  }
+};
 </script>
-
-<style>
-
-</style>
+    
+<style scoped>
+#maincard {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
+</style>>
