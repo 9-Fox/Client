76fa490107142efc3bd5cf2feade4a0ee792fa5b
samuelzega <samuel.zegaa@gmail.com>
2020-01-17 10:21:01 +0700

    resolve 1

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,26 @@
+
 <template>
+<<<<<<< HEAD
   <div id="comment-section">
       <Comment :data="postData"></Comment>
+=======
+  <div >
+    <b-container class="bv-example-row mb-3">
+      <b-row cols="2" align-h="around">
+          <MainCard></MainCard>
+          <MainCard></MainCard>
+          <MainCard></MainCard>
+          <MainCard></MainCard>
+          <MainCard></MainCard>
+      </b-row>
+      <b-row></b-row>
+    </b-container>
+>>>>>>> tampilannya udah
   </div>
 </template>
 
 <script>
+<<<<<<< HEAD
 import Comment from "./components/comment"
 export default {
 name: "App",
@@ -23,8 +39,32 @@ methods:{
   }
 }
 }
+=======
+import MainCard from "./components/MainCard";
+export default {
+  name: "App",
+  components: {
+    MainCard
+  },
+  data(){
+    return {
+      sample: 'ch'
+    }
+  },
+  methods:{
+    printSample(payload){
+      console.log('masuk ga?')
+      this.sample = payload
+    }
+  }
+};
+>>>>>>> tampilannya udah
 </script>
-
-<style>
-
-</style>
+    
+<style scoped>
+#maincard {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
+</style>>
---
+++ src/components/MainCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="m-2">
+      <h3>Name</h3>
+    <b-card
+      overlay
+      :img-src="gambur"
+      img-alt="Card Image"
+      text-variant="white"
+    ></b-card>
+    <span class="spanduk h1 mb-2 ">
+      <b-icon icon="alert-circle-fill" variant="success"></b-icon>
+    </span>
+    <span class="spanduk h1 mb-2">
+      <b-icon icon="alert-circle-fill" variant="success"></b-icon>
+    </span>
+    <span class="spanduk h1 mb-2">
+      <b-icon icon="alert-circle-fill" variant="success"></b-icon>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  nmae: "MainCard",
+  data() {
+    return {
+      sample: "This is a sample text",
+      gambur: "https://image.shutterstock.com/image-photo/white-transparent-leaf-on-mirror-260nw-1029171697.jpg"
+    };
+  },
+  methods: {
+      sending(){
+          this.$emit('send-sample', this.sample)
+          console.log(this.sample)
+      }
+  }
+};
+</script>
+
+<style scoped>
+.spanduk {
+  cursor: pointer;
+}
+</style>
