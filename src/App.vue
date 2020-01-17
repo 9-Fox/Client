045516452a1b7ac7091893afeae6bb07ccbf90db
samuelzega <samuel.zegaa@gmail.com>
2020-01-17 12:38:15 +0700

    add modal postingan sudah bisa

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,31 @@
 
 <template>
+  <div>
     <b-container class="bv-example-row mb-3">
-      <b-row cols="2" align-h="around" v-if="!postselected">
+        <b-row cols="2" align-h="around" v-if="!postselected">
+        <b-button v-b-modal.modal-1>Launch demo modal</b-button>
+        <b-modal id="modal-1" title="BootstrapVue">
+          <UploadForm></UploadForm>
+        </b-modal>
           <MainCard v-for="(item, key) in allpostlist" :key="key" :data="item" v-on:selectedPost="showPost($event)"></MainCard>
       </b-row>
       <b-row v-else>
           <button v-on:click="clearSelected">back</button>
           <MainCard :data="postselected"></MainCard>
           <Comment :data="postselected"></Comment>
-      </b-row>
     </b-container>
+  </div>
 </template>
 
 <script>
-import Comment from "./components/comment"
+import Comment from "./components/comment";
 import MainCard from "./components/MainCard";
+import UploadForm from "./components/uploadForm";
 export default {
   name: "App",
   components: {
     MainCard,
+    UploadForm,
     Comment
   },
   created(){
@@ -34,12 +41,14 @@ export default {
       console.log(err)
     })
   },
-  data(){
+
+  data() {
     return {
       allpostlist: [],
       postselected: null
     }
   },
+
   methods:{
     clearSelected(){
       this.postselected = null
@@ -53,8 +62,8 @@ export default {
         }
       }
     }
-  }
-};
+  },
+}
 </script>
     
 <style scoped>
---
+++ src/components/uploadForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <!-- <b-form @submit="onSubmit" @reset="onReset" v-if="show"> -->
+    <b-form-input id="input-2" v-model="form.name" required placeholder="Enter Post Title"></b-form-input>
+    <b-form-file @change="onFileChange" v-model="form.file" class="mt-3" plain></b-form-file>
+    <b-img v-if="url" :src="url" fluid alt="Fluid image"></b-img>
+    <!-- </b-form> -->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        file: null,
+        name: ""
+      },
+      url: ""
+    };
+  },
+  methods: {
+    onFileChange(e) {
+      const file = e.target.files[0];
+      this.url = URL.createObjectURL(file)
+    }
+  }
+};
+</script>
+
+<style>
+</style>
