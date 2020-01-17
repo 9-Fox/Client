afccbb22a6d208c02eafe132e291e8c06fb4448e
samuelzega <samuel.zegaa@gmail.com>
2020-01-17 10:15:28 +0700

    update maincard

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,7 @@
   <div >
     <b-container class="bv-example-row mb-3">
       <b-row cols="2" align-h="around">
-          <MainCard></MainCard>
-          <MainCard></MainCard>
-          <MainCard></MainCard>
-          <MainCard></MainCard>
-          <MainCard></MainCard>
+          <MainCard v-for="(item, key) in sample" :key="key" :data="item"></MainCard>
       </b-row>
       <b-row></b-row>
     </b-container>
@@ -23,7 +19,31 @@ export default {
   },
   data(){
     return {
-      sample: 'ch'
+      sample: [
+        {
+        title: 'Daun',
+        Like: 1,
+      gambur: "https://image.shutterstock.com/image-photo/white-transparent-leaf-on-mirror-260nw-1029171697.jpg"
+
+        },
+        {
+        title: 'Anjing Lagi Lari, Ini Lucu ga??',
+        Like: 5,
+      gambur: "https://www.animatedimages.org/data/media/202/animated-dog-image-0175.gif"
+        },
+        {
+        title: 'Gatau Mau bikin Judul APa',
+        Like: 12,
+        gambur: "https://si.wsj.net/public/resources/images/JR-AA461B_IFVOL_P_20191031165108.jpg"
+
+        },
+        {
+        title: 'Entah apa ini',
+        Like: 19,
+      gambur: "https://www.animatedimages.org/data/media/140/animated-love-image-0187.gif"
+
+        }
+      ]
     }
   },
   methods:{
